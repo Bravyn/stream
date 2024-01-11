9a6d8cb61923666343157fc54218986dc6ae8c40
Ian Bravyn <ianbravynsa@gmail.com>
2024-01-11 23:02:32 +0300

    Initial commit

---
+++ rekomender.py
@@ -0,0 +1,57 @@
+import streamlit as st
+st.set_page_config(" rekomender ")
+global movies
+movies = ["The Butterfly Effect", "The Light Effect", "The Men", "CyberGirl" ]
+def reco():
+    st.title("M.I Movie Recommender")
+
+
+def get_movie_title():
+    movie_name = st.text_input("Type movie name here")
+    return movie_name
+
+def show_movies():
+    st.subheader(":blue[**RECOMMENDATIONS**]")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.caption("**The Butterfly Effect**")
+        st.image("movie1.jpg", width=300)
+        st.caption("The Men")
+        st.image("movie2.jpg", width=300)
+
+    with col2:
+        st.caption("The Light Effect")
+        st.image("movie3.jpg", width=300)
+        st.caption("CyberGirl")
+        st.image("movie4.jpg", width=300)
+
+reco()
+show = False
+c1, c2 = st.columns([.7, .3])
+with c1:
+    movie = get_movie_title()
+with c2:
+    st.caption("")
+    st.caption("")
+    if movie:
+            #st.info("Looking for recommendations for {}".format(movie.capitalize()))
+            for movi in movies:
+                if movi.lower() == movie.lower():
+                    show = True
+            
+    if st.button("Search"):
+        if movie:
+            #st.info("Looking for recommendations for {}".format(movie.capitalize()))
+            for movi in movies:
+                if movi.lower() == movie.lower():
+                    show = True
+            
+        
+ 
+                   
+
+if show == True:
+    show_movies() 
+else:
+    st.warning('Not found')
+
