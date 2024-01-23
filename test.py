01a0c11dcba11ac9aebf954f649304f3e5b45d8d
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-01-23 13:46:12 +0300

    .

--- test.py
+++ test.py
@@ -14,11 +14,11 @@ global movies
 
 cv = CountVectorizer(max_features=5000, stop_words="english")
 
-df = pd.read_csv("/home/paul/Mindscope/Mindscope/stream/models/final_data.csv")
+df = pd.read_csv("/home/paul/Mindscope/stream/models/final_data.csv")
 
-model_vector = pd.read_pickle(r"/home/paul/Mindscope/Mindscope/stream/models/similar.pkl"
+model_vector = pd.read_pickle(r"/home/paul/Mindscope/stream/models/similar.pkl"
 )
-movies = pd.read_pickle(r"/home/paul/Mindscope/Mindscope/stream/models/movies.pkl")
+movies = pd.read_pickle(r"/home/paul/Mindscope/stream/models/movies.pkl")
 df = df.drop(["Unnamed: 0"], axis=1)
 
 
@@ -26,6 +26,8 @@ model_vector = cv.fit_transform(df["tags"]).toarray()
 
 similar = cosine_similarity(model_vector)
 
+def reco():
+    st.title("M.I Movie Recommender")
 
 # Creating our Recommend function it will return Top 5 movies back
 def recommender(movie):
@@ -46,3 +48,29 @@ def get_movie_title():
     st.caption("__Try The Butterfly Effect__")
 
     return movie_name
+
+def show_movies():
+    st.subheader(":blue[**RECOMMENDATIONS**]")
+    col1, col2 = st.columns(2)
+    
+    
+reco()
+show = False
+c1, c2 = st.columns([.7, .3])
+with c1:
+    movie = get_movie_title()
+    movie = recommender(movie)
+with c2:
+    st.caption("")
+    st.caption("")
+    if movie:
+        show = True
+    
+    if st.button("Search"):
+        if movie:
+            show = True
+            
+
+if show == True:
+    show_movies()
+
